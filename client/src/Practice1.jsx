2ee1aebbd85f5e3835564027a776ed652d78daec
Palak281202 <141211678+Palak281202@users.noreply.github.com>
2025-06-14 10:15:29 +0530

    first commit

---
+++ client/src/Practice1.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Practice1() {
+  const [text, settext] = useState("palak");
+  const clickhandler = (identifier) => {
+    // if(text === 'palak'){
+    //     settext('jangir')
+    // }
+    // else{
+    //     settext('palak')
+    // }
+    if (identifier === "first") {
+      settext("first button");
+    } else if (identifier === "second") {
+      settext("second button");
+    } else if (identifier === "third") {
+      settext("third button");
+    }
+  };
+  return (
+    <div>
+      <button
+        onClick={(event) => {
+          clickhandler("first", event);
+        }}
+      >
+        {text}
+      </button>
+      <button
+        onClick={(event) => {
+          clickhandler("second", event);
+        }}
+      >
+        {text}
+      </button>
+      <button
+        onClick={(event) => {
+          clickhandler("third", event);
+        }}
+      >
+        {text}
+      </button>
+    </div>
+  );
+}
